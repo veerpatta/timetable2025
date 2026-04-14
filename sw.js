44e81f346be93a88dffd1d4b994088cebb312e29
Veer Patta <veerpattaschool@gmail.com>
2026-04-14 11:40:40 +0530

    Bump cache version to v15 for timetable and static assets

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // Service Worker for Veer Patta Public School Timetable
 // Provides offline-first caching for the page shell and timetable data
 
-const CACHE_NAME = 'vpps-timetable-v14';
-const STATIC_CACHE_NAME = 'vpps-static-v14';
+const CACHE_NAME = 'vpps-timetable-v15';
+const STATIC_CACHE_NAME = 'vpps-static-v15';
 
 // Core resources required for offline shell
 const CORE_ASSETS = [
